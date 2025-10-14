63c2813f4cead20950a86cef6f93f765ff31830f
Jakob Schulz <jakob.schulz@tum.de>
2025-10-14 17:55:24 +0200

    added test for identity on languages

--- tests/mata-extensions.cpp
+++ tests/mata-extensions.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <mata/parser/re2parser.hh>
 #include <abstracton/abstracton.hpp>
 
 TEST_CASE( "Identity on Alphabet correct", "[create_identity]" ) {
@@ -16,5 +17,22 @@ TEST_CASE( "Identity on Alphabet correct", "[create_identity]" ) {
         {'0', '0', '1', '1'},
         {'1', '1', '0', '0'},
         {'1', '1', '1', '1'}
+    });
+}
+
+TEST_CASE("Identity on Language correct", "[create_identity]") {
+    using namespace mata;
+    using namespace mata::nfa;
+    using namespace mata::nft;
+
+    Nfa aut = mata::parser::create_nfa("(a|b)*cb*");
+
+    Nft id {create_identity(aut)};
+
+    REQUIRE(id.get_words(4) == std::set<Word> {
+        {'c', 'c'},
+        {'c', 'c', 'b', 'b'},
+        {'a', 'a', 'c', 'c'},
+        {'b', 'b', 'c', 'c'},
     });
 }
