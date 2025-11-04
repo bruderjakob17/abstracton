19e8a63727bdf58e273650113515bcc5f5073abd
Jakob Schulz <jakob.schulz@tum.de>
2025-11-04 19:12:31 +0100

    fixed bugs in compute_ind

--- src/abstracton.cpp
+++ src/abstracton.cpp
@@ -1,37 +1,37 @@
 #include <mata/nfa/nfa.hh>
 #include <mata/nft/nft.hh>
 #include <mata/nft/builder.hh>
+#include <abstracton/mata_extensions.hpp>
 
 using namespace mata;
 using namespace mata::nfa;
 using namespace mata::nft;
 
-Nft create_identity(const Alphabet& alphabet) {
-    Nft result {};
-    State initial {result.add_state()};
-    result.initial.insert(initial);
-    result.final.insert(initial);
-    result.insert_identity(initial, &alphabet);
-    return result;
-}
-
-Nft create_identity(const Nfa& language) {
-    return mata::nft::builder::from_nfa_with_levels_zero(language);
-}
-
-Nfa project(const Nft& nft, int level) {
-    // TODO add possibility to do padding closure wrt. some padding symbol
-    Nft nft_proj {mata::nft::project_to(nft, level)};
-    return nft_proj.to_nfa_move();
-}
-
 // input: DETERMINISTIC abstraction framework!
+// TODO convert output to debug output
+// TODO exclude all a where V(a) = emptyset (optional, but nicer...)
 Nfa compute_ind(const Nft& abstraction_framework, const Nft& transition_relation, const Alphabet& alphabet) {
     // project_1(Id intersect (V delta complement(inverse(V)))), then complement
+    std::cout << "1" << std::endl;
     Nft v_delta {compose(abstraction_framework, transition_relation)};
-    Nft v_complement {mata::nft::complement(abstraction_framework, alphabet)};
+    std::cout << "abstraction framework:" << std::endl;
+    std::cout << abstraction_framework.print_to_dot(true) << std::endl;
+    std::cout << "transition relation:" << std::endl;
+    std::cout << transition_relation.print_to_dot(true) << std::endl;
+    std::cout << "v delta:" << std::endl;
+    std::cout << v_delta.print_to_dot(true) << std::endl;
+    std::cout << "2" << std::endl;
+    Nft v_complement {mata::ext::complement(abstraction_framework, alphabet)};
+    std::cout << "complement of abstraction framework:" << std::endl;
+    std::cout << v_complement.print_to_dot(true) << std::endl;
     Nft product1 {compose(v_delta, v_complement, 1, 1)};
+    std::cout << "4" << std::endl;
+    std::cout << "v delta v_complement^(-1):" << std::endl;
+    std::cout << product1.print_to_dot(true) << std::endl;
     Nft preprojection {mata::nft::intersection(create_identity(alphabet), product1)};
+    std::cout << "5" << std::endl;
+    std::cout << "preprojection:" << std::endl;
+    std::cout << preprojection.print_to_dot(true) << std::endl;
     return mata::nfa::complement(project(preprojection, 0), alphabet);
 }
 Nft compute_preach(const Nft& abstraction_framework, const Nft& transition_relation, const Alphabet& alphabet) {
