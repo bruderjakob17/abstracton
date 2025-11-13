4a3cf86b85a8fa1db92688178b58baf37b2e3dff
Jakob Schulz <jakob.schulz@tum.de>
2025-11-13 16:49:07 +0100

    add argument to remove empty abstractions from ind result

--- include/abstracton/abstracton.hpp
+++ include/abstracton/abstracton.hpp
@@ -2,7 +2,7 @@
 #include <mata/nft/nft.hh>
 
 // input: DETERMINISTIC abstraction framework!
-mata::nfa::Nfa compute_ind(const mata::nft::Nft& abstraction_framework, const mata::nft::Nft& transition_relation, const mata::Alphabet& alphabet);
+mata::nfa::Nfa compute_ind(const mata::nft::Nft& abstraction_framework, const mata::nft::Nft& transition_relation, const mata::Alphabet& alphabet, bool exclude_empty_abstractions = false);
 mata::nft::Nft compute_preach(const mata::nft::Nft& abstraction_framework, const mata::nft::Nft& transition_relation, const mata::Alphabet& alphabet);
 
 
