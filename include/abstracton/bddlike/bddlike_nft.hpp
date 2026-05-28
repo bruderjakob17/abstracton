11144a28b931ab1e033949d6f30542b9616f3f4d
Jakob Schulz <jakob.schulz@tum.de>
2026-05-28 14:22:31 +0200

    finished first version of VecAlphabet implementations, including tests

--- include/abstracton/bddlike/bddlike_nft.hpp
+++ include/abstracton/bddlike/bddlike_nft.hpp
@@ -5,13 +5,14 @@
 #include <mata/nft/delta.hh>
 #include <abstracton/utils/utils.hpp>
 #include <mata/nft/types.hh>
+#include <format>
 
 namespace mata::ext {
 
 class VecAlphabetPrinter {
 public:
-    size_t alphabet_size; // number of tapes this alphabet needs
-    VecAlphabetPrinter(size_t alphabet_size) : alphabet_size(alphabet_size) {};
+    size_t dimension; // number of tapes this alphabet needs
+    VecAlphabetPrinter(size_t dimension) : dimension(dimension) {};
     virtual std::string print(const std::vector<Symbol>& bits) = 0;
     virtual ~VecAlphabetPrinter() = default;
 };
@@ -19,16 +20,19 @@ public:
 /**
  * T is the type used to represent a "Big Symbol", that then gets decomposed into a vec of "small symbols".
  * E.g. T might be an integer (Big Symbol), that gets decomposed into its binary digits (small symbols).
+ *
+ * T can be thought of as "the type through which I want to interact with the automaton".
+ *
  */
 template<typename T>
 class VecAlphabet : public VecAlphabetPrinter {
 public:
-    VecAlphabet(size_t alphabet_size) : VecAlphabetPrinter(alphabet_size) {}
+    VecAlphabet(size_t dimension) : VecAlphabetPrinter(dimension) {}
 
     virtual std::vector<Symbol> translate_symbol(const T& symbol) = 0;
     // virtual std::vector<Symbol> translate_word(const std::vector<T>& word);
 
-    virtual T reverse_translate_symbol(std::vector<Symbol> symbol) = 0;
+    virtual T reverse_translate_symbol(const std::vector<Symbol>& symbol) = 0;
 
     std::vector<Symbol> operator[](const T& symbol) { return this->translate_symbol(symbol); }
 
@@ -40,21 +44,23 @@ public:
  *
  * Because of the general framework, one still needs to wrap the small symbol into a vec, e.g. 5 gets represented as [5].
  *
- * TODO: code is a bit inconsistent: in SupersetVecAlphabet, one translates the symbols w.r.t. the alphabet, but here, no translation is done...
- * THIS SHOULD BE FIXED HERE!!!
+ * Assume the base_alphabet encodes "ef" as 4 and "bab" as 5. Then the functions map as follows:
+ * - translate_symbol: "ef" -> [4]
+ * - reverse_translate_symbol: [4] -> "ef"
+ * - print: [4] -> "ef"
  */
-class SimpleAlphabet : public VecAlphabet<Symbol> {
+class SimpleVecAlphabet : public VecAlphabet<std::string> {
 private:
-    Alphabet* base_alphabet;
+    std::shared_ptr<Alphabet> base_alphabet;
 public:
-    explicit SimpleAlphabet(Alphabet* base_alphabet) : VecAlphabet(1), base_alphabet(std::move(base_alphabet)) {}
+    explicit SimpleVecAlphabet(std::shared_ptr<Alphabet> base_alphabet) : VecAlphabet(1), base_alphabet(std::move(base_alphabet)) {}
 
-    std::vector<Symbol> translate_symbol(const Symbol& symbol) override {
-        return {symbol};
+    std::vector<Symbol> translate_symbol(const std::string& symbol) override {
+        return {base_alphabet->translate_symb(symbol)};
     }
-    Symbol reverse_translate_symbol(std::vector<Symbol> symbol) override {
+    std::string reverse_translate_symbol(const std::vector<Symbol>& symbol) override {
         assert(symbol.size() == 1);
-        return symbol[0];
+        return base_alphabet->reverse_translate_symbol(symbol[0]);
     }
     std::string print(const std::vector<Symbol>& bits) override {
         assert(bits.size() == 1);
@@ -63,17 +69,22 @@ public:
 };
 
 /**
- * In DefaultVecAlphabet(alphabet_size), a big symbol is just a vector of the small symbols it represents,
- * e.g. {'0', '1', '1'} represents the sequence {'0', '1', '1'}.
+ * In DefaultVecAlphabet(dimension), a big symbol is just a vector of the small symbols it represents,
+ * e.g. [0, 15, 1] represents the sequence [0, 15, 1].
+ *
+ * The functions map as follows:
+ * - translate_symbol: [0, 15, 1] -> [0, 15, 1]
+ * - reverse_translate_symbol: [0, 15, 1] -> [0, 15, 1]
+ * - print: [0, 15, 1] -> "[0, 15, 1]"
  */
 class DefaultVecAlphabet : public VecAlphabet<std::vector<Symbol>> {
 public:
-    explicit DefaultVecAlphabet(size_t alphabet_size) : VecAlphabet(alphabet_size) {}
+    explicit DefaultVecAlphabet(size_t dimension) : VecAlphabet(dimension) {}
 
     std::vector<Symbol> translate_symbol(const std::vector<Symbol>& symbol) override {
         return symbol;
     }
-    std::vector<Symbol> reverse_translate_symbol(std::vector<Symbol> symbol) override {
+    std::vector<Symbol> reverse_translate_symbol(const std::vector<Symbol>& symbol) override {
         return symbol;
     }
     std::string print(const std::vector<Symbol>& bits) override {
@@ -82,22 +93,23 @@ public:
 };
 
 /**
- * A SupersetVecAlphabet uses the characteristic sequence to represent a set.
+ * A PowersetVecAlphabet uses the characteristic sequence to represent a set.
  *
- * E.g. in the (ordered) alphabet {"ab", "c", "d"}, the set {"ab", "c"} would be represented as {1, 1, 0}.
+ * Assume the base_alphabet is ["ab", "c", "d"]. Then the functions map as follows:
+ * - translate_symbol: ["ab", "c"] -> [1, 1, 0]
+ * - reverse_translate_symbol: [1, 1, 0] -> ["ab", "c"]
+ * - print: [1, 1, 0] -> "[ab, c]"
  */
-class SupersetVecAlphabet : public VecAlphabet<std::vector<std::string>> {
+class PowersetVecAlphabet : public VecAlphabet<std::vector<std::string>> {
 private:
-    Alphabet* base_alphabet;
-    size_t base_alphabet_size;
+    std::shared_ptr<Alphabet> base_alphabet;
 public:
-    explicit SupersetVecAlphabet(Alphabet* base_alphabet) : VecAlphabet(base_alphabet->get_alphabet_symbols().size()), base_alphabet(std::move(base_alphabet)) {
-        base_alphabet_size = base_alphabet->get_alphabet_symbols().size();
+    explicit PowersetVecAlphabet(std::shared_ptr<Alphabet> base_alphabet) : VecAlphabet(base_alphabet->get_alphabet_symbols().size()), base_alphabet(std::move(base_alphabet)) {
     }
 
-    std::vector<std::string> reverse_translate_symbol(std::vector<Symbol> symbol) override {
+    std::vector<std::string> reverse_translate_symbol(const std::vector<Symbol>& symbol) override {
         std::vector<std::string> result{};
-        for (int i{ 0 }; i < base_alphabet_size; ++i) {
+        for (int i{ 0 }; i < dimension; ++i) {
             if (symbol[i] == 1) {
                 result.push_back(base_alphabet->reverse_translate_symbol(i));
             }
@@ -105,7 +117,7 @@ public:
         return result;
     }
     std::vector<Symbol> translate_symbol(const std::vector<std::string>& set) override {
-        std::vector<Symbol> result(base_alphabet_size, 0);
+        std::vector<Symbol> result(dimension, 0);
         for (const std::string& s : set) {
             result[base_alphabet->translate_symb(s)] = 1;
         }
@@ -113,6 +125,98 @@ public:
     }
     std::string print(const std::vector<Symbol>& bits) override {
         return vec_to_string(reverse_translate_symbol(bits));
+    }
+};
+
+/**
+ * Use fixed-length base encoding (most significant digit first).
+ *
+ * Assume base = 2, length = 4. Then the functions map as follows:
+ * - translate_symbol: 5 -> [0, 1, 0, 1]
+ * - reverse_translate_symbol: [0, 1, 0, 1] -> 5
+ * - print: [0, 1, 0, 1] -> "5"
+ */
+class BaseMSDVecAlphabet : public VecAlphabet<long> {
+private:
+    size_t base;
+public:
+    explicit BaseMSDVecAlphabet(size_t base, size_t length) : VecAlphabet(length), base(base) {}
+
+    std::vector<Symbol> translate_symbol(const long& symbol) override {
+        // map e.g. 5 -> [0, 1, 0, 1] if base = 2 and length = 4
+        std::vector<Symbol> digits(dimension, 0);
+        long number = symbol;
+
+        int i = dimension - 1;
+        while (number > 0) {
+            if (i < 0) {
+                throw std::runtime_error(std::format("error: cannot represent {} in base {} using {} digits", symbol, base, dimension));
+            }
+            digits[i] = number % base;
+            number /= base;
+            --i;
+        }
+
+        return digits;
+    }
+    long reverse_translate_symbol(const std::vector<Symbol>& bits) override {
+        // map e.g. [0, 1, 0, 1] -> 5 if base = 2 and length = 4
+        long result{0};
+
+        for (int i{0}; i < bits.size(); ++i) {
+            result = base * result + bits[i];
+        }
+
+        return result;
+    }
+    std::string print(const std::vector<Symbol>& bits) override {
+        return std::to_string(reverse_translate_symbol(bits));
+    }
+};
+
+/**
+ * Use fixed-length base encoding (most significant digit first).
+ *
+ * Assume base = 2, length = 4. Then the functions map as follows:
+ * - translate_symbol: 5 -> [1, 0, 1, 0]
+ * - reverse_translate_symbol: [1, 0, 1, 0] -> 5
+ * - print: [1, 0, 1, 0] -> "5"
+ */
+class BaseLSDVecAlphabet : public VecAlphabet<long> {
+private:
+    size_t base;
+public:
+    explicit BaseLSDVecAlphabet(size_t base, size_t length) : VecAlphabet(length), base(base) {}
+
+    std::vector<Symbol> translate_symbol(const long& symbol) override {
+        // map e.g. 5 -> [1, 0, 1, 0] if base = 2 and length = 4
+        std::vector<Symbol> digits(dimension, 0);
+        long number = symbol;
+
+        int i = 0;
+        while (number > 0) {
+            if (i >= dimension) {
+                throw std::runtime_error(std::format("error: cannot represent {} in base {} using {} digits", symbol, base, dimension));
+            }
+            digits[i] = number % base;
+            number /= base;
+            ++i;
+        }
+
+        return digits;
+    }
+    long reverse_translate_symbol(const std::vector<Symbol>& bits) override {
+        // map e.g. [1, 0, 1, 0] -> 5 if base = 2 and length = 4
+        long result{0};
+
+        for (int i{static_cast<int>(bits.size()) - 1}; i >= 0; --i) {
+            result = base * result + bits[i];
+        }
+
+        return result;
+    }
+    std::string print(const std::vector<Symbol>& bits) override {
+        return std::to_string(reverse_translate_symbol(bits));
     }
 };
 
--- tests/bddlike/bddlike_nft.cpp
+++ tests/bddlike/bddlike_nft.cpp
@@ -5,9 +5,114 @@
 #include <abstracton/mata_extensions.hpp>
 #include <abstracton/utils/utils.hpp>
 
-TEST_CASE( "bddlike_nft default alphabet", "[bddlike_nft]" ) {
-    using namespace mata;
+TEST_CASE( "DefaultVecAlphabet", "[DefaultVecAlphabet]" ) {
+    using namespace mata::ext;
 
-    ext::BDDlikeNft aut = ext::BDDlikeNft::with_alphabet_sizes({2, 3});
-    REQUIRE(aut.levels.num_of_levels == 5);
+    DefaultVecAlphabet alph = DefaultVecAlphabet(3);
+    CHECK(alph.dimension == 3);
+    CHECK(alph.translate_symbol({0, 15, 1}) == std::vector<mata::Symbol>{0, 15, 1});
+    CHECK(alph.reverse_translate_symbol({0, 15, 1}) == std::vector<mata::Symbol>{0, 15, 1});
+    CHECK(alph.print({0, 15, 1}) == std::string("[0, 15, 1]"));
+}
+
+TEST_CASE( "SimpleVecAlphabet", "[SimpleVecAlphabet]" ) {
+    using namespace mata;
+    using namespace mata::ext;
+    using namespace std;
+
+    SECTION("EnumAlphabet") {
+        EnumAlphabet alphabet = {'#', 'a', 'b'};
+
+        // WARNING EnumAlphabet is weird... 'a' = 97, so to access it, one must use the string "97":
+        SimpleVecAlphabet alph = SimpleVecAlphabet(make_shared<EnumAlphabet>(alphabet));
+        CHECK(alph.dimension == 1);
+        CHECK(alph.translate_symbol("97") == vector<Symbol>{'a'});
+        CHECK(alph.reverse_translate_symbol({'a'}) == string("97"));
+        CHECK(alph.print({'a'}) == string("97"));
+    }
+
+    SECTION("OnTheFlyAlphabet") {
+        OnTheFlyAlphabet alphabet{vector<string>{"ab", "c", "d", "ef", "bab"}};
+
+        SimpleVecAlphabet alph = SimpleVecAlphabet(make_shared<OnTheFlyAlphabet>(alphabet));
+        CHECK(alph.dimension == 1);
+        CHECK(alph.translate_symbol("ef") == vector<Symbol>{3});
+        CHECK(alph.reverse_translate_symbol({3}) == string("ef"));
+        CHECK(alph.print({3}) == string("ef"));
+    }
+}
+
+TEST_CASE( "PowersetVecAlphabet", "[PowersetVecAlphabet]" ) {
+    using namespace mata;
+    using namespace mata::ext;
+    using namespace std;
+
+    OnTheFlyAlphabet alphabet{vector<string>{"ab", "c", "d"}};
+
+    PowersetVecAlphabet alph = PowersetVecAlphabet(make_shared<OnTheFlyAlphabet>(alphabet));
+    CHECK(alph.dimension == 3);
+    CHECK(alph.translate_symbol({"ab", "c"}) == vector<Symbol>{1, 1, 0});
+    CHECK(alph.reverse_translate_symbol({1, 1, 0}) == vector<string>{"ab", "c"});
+    CHECK(alph.print({1, 1, 0}) == "[ab, c]");
+}
+
+TEST_CASE( "BaseMSDVecAlphabet", "[BaseMSDVecAlphabet]") {
+    using namespace mata;
+    using namespace mata::ext;
+    using namespace std;
+
+    BaseMSDVecAlphabet alph = BaseMSDVecAlphabet(2, 4);
+    CHECK(alph.dimension == 4);
+
+    // 5
+    CHECK(alph.translate_symbol(5) == vector<Symbol>{0, 1, 0, 1});
+    CHECK(alph.reverse_translate_symbol({0, 1, 0, 1}) == 5);
+    CHECK(alph.print({0, 1, 0, 1}) == "5");
+
+    // 11
+    CHECK(alph.translate_symbol(11) == vector<Symbol>{1, 0, 1, 1});
+    CHECK(alph.reverse_translate_symbol({1, 0, 1, 1}) == 11);
+    CHECK(alph.print({1, 0, 1, 1}) == "11");
+
+    // 0
+    CHECK(alph.translate_symbol(0) == vector<Symbol>{0, 0, 0, 0});
+    CHECK(alph.reverse_translate_symbol({0, 0, 0, 0}) == 0);
+    CHECK(alph.print({0, 0, 0, 0}) == "0");
+
+    // 16 (too large symbol)
+    CHECK_THROWS(alph.translate_symbol(16));
+}
+
+TEST_CASE( "BaseLSDVecAlphabet", "[BaseLSDVecAlphabet]") {
+    using namespace mata;
+    using namespace mata::ext;
+    using namespace std;
+
+    BaseLSDVecAlphabet alph = BaseLSDVecAlphabet(2, 4);
+    CHECK(alph.dimension == 4);
+
+    // 5
+    CHECK(alph.translate_symbol(5) == vector<Symbol>{1, 0, 1, 0});
+    CHECK(alph.reverse_translate_symbol({1, 0, 1, 0}) == 5);
+    CHECK(alph.print({1, 0, 1, 0}) == "5");
+
+    // 11
+    CHECK(alph.translate_symbol(11) == vector<Symbol>{1, 1, 0, 1});
+    CHECK(alph.reverse_translate_symbol({1, 1, 0, 1}) == 11);
+    CHECK(alph.print({1, 1, 0, 1}) == "11");
+
+    // 0
+    CHECK(alph.translate_symbol(0) == vector<Symbol>{0, 0, 0, 0});
+    CHECK(alph.reverse_translate_symbol({0, 0, 0, 0}) == 0);
+    CHECK(alph.print({0, 0, 0, 0}) == "0");
+
+    // 16 (too large symbol)
+    CHECK_THROWS(alph.translate_symbol(16));
+}
+
+TEST_CASE( "bddlike_nft default alphabet", "[bddlike_nft]" ) {
+    using namespace mata::ext;
+
+    BDDlikeNft aut = BDDlikeNft::with_alphabet_sizes({2, 3});
+    CHECK(aut.levels.num_of_levels == 5);
 }
