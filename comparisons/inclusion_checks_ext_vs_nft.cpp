5cb1b0a4324320027ba894f1f3c845669da6f5b6
Jakob Schulz <jakob.schulz@tum.de>
2026-07-21 17:16:25 +0200

    add comparison of inclusion checks

---
+++ comparisons/inclusion_checks_ext_vs_nft.cpp
@@ -0,0 +1,59 @@
+#include <abstracton/mata_extensions.hpp>
+#include <chrono>
+#include <random>
+#define INIT_CLOCKS() std::chrono::steady_clock::time_point begin, end;
+#define TICK() begin = std::chrono::steady_clock::now();
+#define TOCK(message) end = std::chrono::steady_clock::now(); std::cout << "Measured time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+#define TOCKM(message) end = std::chrono::steady_clock::now(); std::cout << "Time needed for " << message << ": " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+
+
+int main() {
+    for (int i = 0; i < 5; ++i) {
+        size_t num_of_levels = 2;
+        size_t num_of_states = 200;
+        std::vector<size_t> alphabet_sizes = {2, 3};
+        double states_trans_ratio_per_symbol = 10.0;
+        double final_state_density = 0.1;
+        std::optional<unsigned int> seed = std::make_optional(std::random_device{}());
+        mata::nft::Nft aut = mata::ext::builder::create_random_nft_tabakov_vardi(
+            num_of_levels,
+            num_of_states,
+            alphabet_sizes,
+            states_trans_ratio_per_symbol,
+            final_state_density,
+            seed
+        );
+
+        mata::EnumAlphabet alph0 = {0, 1};
+        mata::EnumAlphabet alph1 = {0, 1, 2};
+        std::vector<mata::Alphabet*> alphabets = {&alph0, &alph1};
+        mata::nft::Run cex0, cex1, cex2;
+
+        std::cout << "Seed: " << seed.value() << std::endl;
+
+        INIT_CLOCKS();
+
+        std::cout << "Default:" << std::endl;
+        TICK();
+        bool is_universal0 = mata::ext::is_universal_antichains(aut, alphabets, &cex0);
+        TOCK();
+        std::cout << "Result: " << is_universal0 << std::endl;
+
+        std::cout << "Using inclusion:" << std::endl;
+        TICK();
+        bool is_universal1 = mata::ext::is_universal_antichains_by_inclusion(aut, alphabets, &cex1);
+        TOCK();
+        std::cout << "Result: " << is_universal1 << std::endl;
+
+        std::cout << "Lazy (without subsumption):" << std::endl;
+        TICK();
+        bool is_universal2 = mata::ext::is_universal_lazy(aut, alphabets, &cex2);
+        TOCK();
+        std::cout << "Result: " <<is_universal2 << std::endl;
+
+        if (is_universal0 != is_universal1 || is_universal0 != is_universal2) {
+            std::cout << "ERROR: different answers for seed " << seed.value() << std::endl;
+            return 0;
+        }
+    }
+}
